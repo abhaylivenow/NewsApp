2cc0bcadcc95cc0ab86cb8f69c57511499416979
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2022-01-25 23:56:51 +0530

    Retrofit instance class created.

---
+++ app/src/main/java/com/example/newsapp/api/NewsApi.kt
@@ -0,0 +1,30 @@
+package com.example.newsapp.api
+
+import com.example.newsapp.model.NewsResponse
+import com.example.newsapp.utils.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("v2/top-headlines")
+    suspend fun getNews(
+        @Query("country")
+        countryCode: String = "in",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+}
---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val API_KEY = "b4fde7ed09e64358b6e0c00da8a39b72"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
