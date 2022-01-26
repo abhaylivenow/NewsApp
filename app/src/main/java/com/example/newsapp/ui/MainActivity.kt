aa46a3b355398235fe1accd6fd872d8f44254003
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2022-01-27 01:00:20 +0530

    News is getting fetched from server and shown in recycler view.

--- app/src/main/java/com/example/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/MainActivity.kt
@@ -2,21 +2,72 @@ package com.example.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.R
+import com.example.newsapp.adapters.NewsAdapter
 import com.example.newsapp.db.ArticleDatabase
 import com.example.newsapp.repository.NewsRepository
+import com.example.newsapp.utils.Resource
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    private lateinit var progressBar: ProgressBar
+    private lateinit var rvNews: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        progressBar = findViewById(R.id.progressBar)
+        rvNews = findViewById(R.id.rv_news)
+        setupRecyclerView()
+
         val newsRepository = NewsRepository(ArticleDatabase(this))
         val viewModelProviderFactory = NewsViewModelProvider(newsRepository)
         viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+        viewModel.breakingNews.observe(this, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Toast.makeText(applicationContext,"Error occurred!",Toast.LENGTH_SHORT).show()
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun hideProgressBar() {
+        progressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(applicationContext)
+        }
     }
 }
